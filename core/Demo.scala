2117d03a54cc2ac9aad9184583f4342342aca48e
Adrien Ghosn <ghosn.adrien@gmail.com>
2015-01-16 01:54:21 +0100

    Small modifications

--- core/Demo.scala
+++ core/Demo.scala
@@ -2,8 +2,15 @@ object Demo {
   implicit val i = 3
   implicit def foo = "Hello"
   val l = List(1, 2, 3, 4).toSet
-  def badUnit { println("Hello World!")}
+  def badUnit { 
+    println("Hello World!")
+  }
   def bar(arg: String, more: String*) {
     println("Hello "+arg)
   } 
+  var legal = "shouldn't be modified"
+  implicit def dummy = {
+    var c = "should be a val"
+  }
+  
 }
