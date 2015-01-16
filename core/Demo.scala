33dd731fa884335b9fa3c4195b04e1d31385c1ad
Adrien Ghosn <ghosn.adrien@gmail.com>
2015-01-16 11:56:39 +0100

    Modified the example

--- core/Demo.scala
+++ core/Demo.scala
@@ -1,16 +1,15 @@
 object Demo {
   implicit val i = 3
   implicit def foo = "Hello"
-  val l = List(1, 2, 3, 4).toSet
   def badUnit { 
-    println("Hello World!")
+    val hw = "Hello World!"
   }
   def bar(arg: String) {
-    println("Hello "+arg)
+    var s = "Hello "+arg
   } 
-  var legal = "shouldn't be modified"
-  implicit def dummy = {
-    var c = "should be a val"
+  var legal = "No problem here"
+  implicit def dummy {
+    var c = "I should be a val"
   }
   
 }
